8488d24df5f7f8abf67e253b007d7fe2af54234f
Daniel <50612327+daniel2IT@users.noreply.github.com>
2020-10-31 20:38:20 +0200

    Modification

--- XirYzaidimas/Start.Designer.cs
+++ XirYzaidimas/Start.Designer.cs
@@ -110,7 +110,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(204, 247);
+            this.ClientSize = new System.Drawing.Size(234, 255);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
